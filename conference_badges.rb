3759a70aff7d04c74b5962b48b232e01efbbda2d
Taemin Kim <taeminkimny@gmail.com>
2022-07-01 13:52:44 -0400

    commit tree 573b4028525ce886c4d528c810eef86e5c0703b4

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  return arr.map{|name| "Hello, my name is #{name}."}
+end 
+
+def assign_rooms(arr)
+  arr.map.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer (arr)
+  batch_badge_creator(arr).each do |name|
+    puts name
+  end
+  assign_rooms(arr).each do |assignment|
+    puts assignment
+  end
+end
